2df029bedb965f10bc81c15dfcd6d5c6a3c0788d
makiton <makiton@gmail.com>
2021-09-16 12:36:17 +0900

    Reformat

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ set -uex
 
 # Installing homebrew
 echo "Installing homebrew"
-if type brew > /dev/null; then
-  echo "homebrew is already installed";
+if type brew >/dev/null; then
+  echo "homebrew is already installed"
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# install brews 
+# install brews
 brew bundle
 
-# Install dotfiles 
+# Install dotfiles
 if [ ! -e ../dotfiles ]; then
   gh repo clone makiton/dotfiles ../dotfiles
   $(cd ../dotfiles && ./install.sh)
 fi
 
 # Install dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/dein-installer.sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >/tmp/dein-installer.sh
 sh /tmp/dein-installer.sh ~/.cache/dein/
